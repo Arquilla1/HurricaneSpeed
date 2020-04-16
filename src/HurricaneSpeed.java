53256bbc8887b1a0eafc9541ff675cf5030a4346
Arquilla1 <Arquilla1@msn.com>
2020-04-16 11:09:51 -0400

    first commit

---
+++ src/HurricaneSpeed.java
@@ -0,0 +1,52 @@
+/*  */
+
+
+
+
+
+import java.util.Scanner;
+
+public class HurricaneSpeed {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+int speed = 0;
+        while (true) {
+            System.out.println("Enter the speed");
+            speed = s.nextInt();
+            if ((speed >= 74) && (speed <= 95)) {
+                System.out.println("Category 1");
+
+            }
+        else if ((speed >=96) && (speed <=110)) {
+                System.out.println("Category 2");
+
+            }else if ((speed >= 111) && (speed <=129)) {
+                System.out.println("Category 3");
+
+            }else if ((speed >= 130) && (speed <=156)){
+                        System.out.println("Category 4");
+
+                     if ((speed >= 157))
+                            System.out.println("Category 5");
+
+                        } else if ((speed >= 130))
+                        System.out.println("Category 4");
+
+                        else if ((speed >= 111)) {
+                            System.out.println("Category 3");
+
+                        } else if ((speed >= 96)) {
+                            System.out.println("Category 2");
+
+                        } else  if ((speed >= 74)) {
+                            System.out.println("Category 1");
+
+                        } else {
+                        System.out.println("too weak  ");
+
+
+
+        }}}}
+
+
+
